be4ff6506353541dd92fad2cc36dd03ed5819872
AlenChettiar <alen.demello1981@gmail.com>
2024-09-28 12:22:21 +0530

    add: AlenChettiar entry

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "AlenChettiar",
+    name: "Alen Chettiar",
+    quote: "BRUH !!!",
+  },
 ];
